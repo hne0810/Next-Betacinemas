7b378eae563846a3650d514af0cc08b5a169e4d4
nguyen hung <hungtccc11@gmail.com>
2025-01-18 01:21:41 +0700

    guide:use children in HomePage and access with loginPage

---
+++ app/fe/components/authenticationPage/login/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import HomePage from '../../homepage'
+
+const LoginPage = () => {
+  return (
+    <HomePage>
+      <div>
+        Login
+      </div>
+    </HomePage>
+  )
+}
+
+export default LoginPage
--- app/fe/components/homepage/index.tsx
+++ app/fe/components/homepage/index.tsx
@@ -1,20 +1,25 @@
-import React from 'react'
+'use client';
+
+import React, { ReactNode } from 'react'
 import Header from './header/header';
 import Footer from './footer';
 
-interface HeaderProps {
-  popup: React.ReactNode;
-}
+// interface HeaderProps {
+//   popup: React.ReactNode;
+// }
 
-const HomePage: React.FC<HeaderProps> = ({popup}) => {
+const HomePage = ({ children}: {children:ReactNode}) => {
   return (
     <div className='bg-white h-screen'>
     <div>
         <Header />
     </div>
-        <div className='flex justify-center'>
-            {popup}
-        </div>
+    {/* <div className='flex justify-center'>
+        {popup}
+    </div> */}
+    <main>
+    {children}  
+    </main>
     <div className='flex mt-[10px] flex-1 w-screen'>
       <Footer />
     </div>
